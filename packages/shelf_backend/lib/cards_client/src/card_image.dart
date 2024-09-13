09f9ad3d04b8f8dc8bf836d372693ec2e104ee7b
Mike Campbell <mikercampbell@outlook.com>
2024-09-12 22:53:11 -0600

    now hosting already downlaoded images

--- packages/shelf_backend/lib/cards_client/src/card_image.dart
+++ packages/shelf_backend/lib/cards_client/src/card_image.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
+import 'dart:typed_data';
 
-void saveImage(String url, String quality, String filetype, String cardID) async {
-  downloadImage("$url/$quality.$filetype", "/Users/mike/Projects/dart/fullstack_pokemon/deps/card-images/$cardID.$filetype");
+void saveImage(String url, String quality, String cardID) async {
+  downloadImage("$url/$quality.png", "/Users/mike/Projects/dart/fullstack_pokemon/deps/card-images/$cardID.png");
 }
 
 void downloadImage(String url, String outputPath) async {
@@ -9,4 +10,15 @@ void downloadImage(String url, String outputPath) async {
   final file = File(outputPath);
   final downloadedImage = await response.close();
   await downloadedImage.pipe(file.openWrite());
+}
+
+Future<Uint8List> loadImage(String cardID) {
+  final file = File("/Users/mike/Projects/dart/fullstack_pokemon/deps/card-images/$cardID.png");
+  if (file.existsSync()) {
+    print("Image exists");
+  } else {
+    print("Image does not exist");
+  }
+
+  return file.readAsBytes();
 }
--- packages/shelf_backend/lib/http/routes/card.dart
+++ packages/shelf_backend/lib/http/routes/card.dart
@@ -1,4 +1,7 @@
+import 'dart:typed_data';
+
 import 'package:postgres/messages.dart';
+import 'package:shelf_backend/cards_client/src/card_image.dart';
 import 'package:shelf_backend/cards_client/src/cards_client_base.dart';
 import 'package:shelf_backend/db/card.dart';
 import 'package:shelf_backend/http/context.dart';
@@ -12,13 +15,29 @@ void addRoutes(Router router, Context ctx) {
     return Response.ok(payload, headers: {'Content-Type': 'application/json'});
   });
 
-  // router.get('/card/<id>/image', (Request request, String id) async {
-  //   final card = await (id);
-  //   return Response.ok(card.toJson());
-  // });
+  router.get('/card/<id>/image', (Request request, String id) async {
+    Uint8List image = await loadImage(id);
+    return Response.ok(image, headers: {'Content-Type': 'image/png'});
+  });
 }
 
 Future<String> getCachedCard(Context ctx, String id) async {
+  try {
+    final cachedCard = await getCachedCardJSON(ctx, id);
+    return cachedCard;
+  } catch (e) {
+    ctx.logger.e(e);
+    if (e.runtimeType == NoDataMessage) {
+      ctx.logger.d('No cached card found');
+      final payload = await getCardJSON(id);
+      await cacheCard(ctx.db, id, payload);
+      return payload.toString();
+    }
+  }
+  return "n/a";
+}
+
+Future<String> getCachedCardImage(Context ctx, String id) async {
   try {
     final cachedCard = await getCachedCardJSON(ctx, id);
     return cachedCard;
